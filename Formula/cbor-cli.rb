6a22afeea108aa41fdc326b43f035eeff519adcb
Dane Stuckel <dane.stuckel@gmail.com>
2024-01-07 22:41:45 +0700

    reduce length of description

--- Formula/cbor-cli.rb
+++ Formula/cbor-cli.rb
@@ -1,5 +1,5 @@
 class CborCli < Formula
-  desc "Command line tool for encoding and decoding CBOR using serde. Supports import and export for JSON, YAML, and TOML. Supports deep inspection of CBOR files."
+  desc "Encode, decode and inspect CBOR data with JSON, YAML, TOML, including stdin/out"
   homepage "https://github.com/takenpilot/cbor-rs"
   url "https://github.com/takenpilot/cbor-rs/archive/refs/tags/v0.4.0.tar.gz"
   sha256 "66fadd96bb769f8abee654e83e4b01357700d14dcfc0c3339dcd5a74a2d0fc9d"
@@ -8,7 +8,6 @@ class CborCli < Formula
   depends_on "rust" => :build
 
   def install
-    # system "cargo", "install", "--path", "cli"
     system "cargo", "build", "--release", "--bin", "cbor"
     bin.install "target/release/cbor"
   end
